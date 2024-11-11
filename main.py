019f2b0aecb12dbe88e389c4d1fbd9cfce01b968
GianiBraga <giani.braga80@gmail.com>
2024-11-11 10:40:51 -0300

    Adicionado listar
    Adicionado função e interface para listar os dados

--- main.py
+++ main.py
@@ -34,15 +34,43 @@ def salvar_dados():
     except sqlite3.Error as erro:
         print("Erro ao inserir os dados: ", erro )
 
+# Criar uma função pra listar os dados na tabela
+def listar_dados():
+
+    tela2.show()
+    
+    # Criando a conexão com o banco de dados
+    banco = sqlite3.connect('cursos.db')
+
+    # Criação do objeto cursor
+    cursor = banco.cursor()
+
+    # Consultando os dados na tabela
+    cursor.execute("SELECT * FROM curso")
+
+    # Armazenando em uma variável
+    dados_lidos = cursor.fetchall()
+
+    # Exibindo os dados na tabela
+    tela2.tableWidget.setRowCount(len(dados_lidos))
+    tela2.tableWidget.setColumnCount(3)
+    
+    # Percorrer uma matriz de dados que seja exibido na tela 
+    for i in range(0, len(dados_lidos)):
+        for j in range(0,3):
+            tela2.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+    
+    banco.close()
 
 
 
 # Estrutura para abrir a página
 app = QtWidgets.QApplication([])
 tela = uic.loadUi("cadastro_curso.ui")
+tela2 = uic.loadUi("listar.ui")
 
 tela.pushButton_salvar.clicked.connect(salvar_dados)
-
+tela.pushButton_listar.clicked.connect(listar_dados)
 
 # Comandos para abrir a tela
 tela.show()
